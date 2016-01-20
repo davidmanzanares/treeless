9037c2ed4020ff6c69168f62b9a3ad9998dc8cdd
David M <manxi.david@gmail.com>
2016-01-20 20:11:28 +0100

    small changes

--- src/com/keepalive.go
+++ src/com/keepalive.go
@@ -4,5 +4,5 @@ package tlcom
 
 type Keepalive struct {
 	KnownChunks  []int    //Chunks known by the server
-	KnownServers []string //Servers known by the server in format ip:port
+	KnownServers []string //Servers known by the server
 }
--- src/com/virtualserver.go
+++ src/com/virtualserver.go
@@ -10,7 +10,7 @@ type VirtualServer struct {
 	Conn          *Conn     //TCP connection, it may not exists
 }
 
-//NeedConnection tries to create a connection to the server
+//NeedConnection tries to create a connection to the server if needed
 func (s *VirtualServer) NeedConnection() (err error) {
 	if s.Conn == nil {
 		s.Conn, err = CreateConnection(s.Phy)
